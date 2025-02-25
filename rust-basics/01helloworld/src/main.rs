ff094563002daeee7dd62e5a7765121c903b98d3
forest-211 <767425412lin@gmail.com>
2025-02-25 23:49:48 +0800

    feat: add practice

--- rust-basics/01helloworld/src/main.rs
+++ rust-basics/01helloworld/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
+
+    let s = "I am a superman.".to_string();
+    // 使用 for 循环遍历 1 到 10 的数字, 使用 _i 表示不使用循环变量
+    for _i in 1..10 {
+        // 使用 &s 创建一个临时的引用
+        let tmp_s = &s;
+        println!("s is {}", tmp_s);
+    }
 }
